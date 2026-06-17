b17daa5a4be60162904ae7c17f5a1ab928cd0cde
Karan Agrahari <114682523+KaranAgraharee@users.noreply.github.com>
2026-06-17 19:57:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://webinar-backend-ib7t.onrender.com',
         changeOrigin: true,
       },
     },
